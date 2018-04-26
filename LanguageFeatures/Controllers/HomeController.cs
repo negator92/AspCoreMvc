2b924bda5b4658ab171f760c71e49d58c0f06499
Vincent Neparidze <negator92@gmail.com>
2018-04-26 17:49:57 +0300

    Page87

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product product in Product.GetProducts())
+            {
+                string name = product?.Name;
+                decimal? price = product?.Price;
+                results.Add($"Name: {name}, Price: {price}");
+            }
+
+            return View(results);
+        }
+    }
+}
