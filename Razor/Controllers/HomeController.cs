4d73490e7cebbb8d6fe8547893f4d0eee43d995b
Vincent Neparidze <negator92@gmail.com>
2018-04-30 07:48:09 +0300

    Page118 return skipped files

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product product = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+            return View(product);
+        }
+    }
+}
