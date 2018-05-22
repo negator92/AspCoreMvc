16cb7a2d6e3e0cdb36b58cfa74b0face14fed7c6
Vincent Neparidze <negator92@gmail.com>
2018-05-22 21:24:47 +0300

    Page179

---
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,24 @@
+﻿using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var product = new Product {Name = "Test", Price = 100M};
+            product.Name = "New Name";
+            Assert.Equal("New Name", product.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var product = new Product {Name = "Test", Price = 100M};
+            product.Price = 200M;
+            Assert.Equal(100M, product.Price);
+        }
+    }
+}
