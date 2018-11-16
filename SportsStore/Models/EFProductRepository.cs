b6f6cfed56695097ddb5722fc7c49a60cb7a29f7
Vincent Neparidze <nva@akmetron.ru>
2018-11-16 16:55:32 +0300

    Page212

---
+++ SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFProductRepository(ApplicationDbContext context)
+            => this.context = context;
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
