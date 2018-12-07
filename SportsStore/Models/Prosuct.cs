e4947026f729c49f413b109d6cc4ed94a0995d5e
Vincent Neparidze <nva@akmetron.ru>
2018-12-07 16:41:43 +0300

    Page216

--- SportsStore/Models/Prosuct.cs
+++ SportsStore/Models/Prosuct.cs
@@ -6,6 +6,6 @@ namespace SportsStore.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string Catergory { get; set; }
+        public string Category { get; set; }
     }
 }
